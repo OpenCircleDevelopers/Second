9c835aa217e2a368f5f6f256fb437fe64c3da99e
OpenCircleDeveloper <opencircledevelopers@gmail.com>
2025-05-16 16:13:21 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import MainLayout from './layout/MainLayout'
+import Homepage from './pages/HomePage/Homepage'
+import CreateQuiz from './pages/CreateQuiz'
+
+
+
+const App = () => {
+
+  const Router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="/create-quiz" element={<CreateQuiz />} />
+      </Route>)
+  )
+
+
+  return (
+    <RouterProvider router={Router} />
+  )
+}
+
+export default App
---
+++ src/pages/HomePage/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Style.css'
+import { Link } from 'react-router-dom'
+
+const Homepage = () => {
+    return (
+        <div className='container'>
+            <h1 className='title text-5xl mb-15'>Project Quiz</h1>
+            <Link to="/create-quiz" className='mt-10 border px-5 py-3 rounded text-blue-400 active:text-blue-600' >Create New QUIZ</Link>
+            <div className='quiz-group rounded'>
+                <div className="mx-15 py-5 mt-15">
+                    <h1>Other Quizes</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
