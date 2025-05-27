d9d41f2a0cac3e6f49ac03af10719883c1b829a9
OpenCircleDeveloper <opencircledevelopers@gmail.com>
2025-05-27 06:03:54 +0500

    radio button functionality

--- src/pages/CreateQuiz.jsx
+++ src/pages/CreateQuiz.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaArrowLeft } from "react-icons/fa"
+import { FaPlus } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+
 
 const CreateQuiz = () => {
+
+    const [quizQuestion, setQuizQuestion] = useState([])
+    const [quizType,setQuizType] = useState('radio')
+
+    const addQuestion = () => {
+        const questionDemo = {
+            type: "text",
+            placeholder: 'please input your question here',
+        }
+        setQuizQuestion([...quizQuestion, questionDemo])
+    }
+
+    const changeHandler = (e) => {
+        setQuizType(e.target.value)
+    }
     return (
-        <div>CreateQuiz</div>
+        <div className='bg-primary w-80 mx-auto mt-15'>
+            <Link to="/" className='inline-block m-4'><FaArrowLeft /></Link>
+            <div className='form'>
+                <input type="text" name="quiz-title" placeholder='Input your title here' />
+                <br />
+                <textarea name="quiz-description" placeholder="input quiz description here"></textarea>
+                <br />
+                <input type="number" name="quiz-marks" placeholder='input full marks of quiz' />
+                <br />
+                <input type="number" name="quiz-time" placeholder='input time of quiz' />
+                <br />
+                <div className='flex'>
+                    <h3 className='inline'>Questions: </h3>
+                    <button className='ml-auto border p-1' onClick={addQuestion}><FaPlus /></button>
+                </div>
+                {quizQuestion.map((quiz, index) => {
+                    return (
+                        <div key={index} className='mt-5'>
+                            <input type={quiz.type} name="quiz-question" placeholder={quiz.placeholder} />
+                            <select name="option-type" className='border ml-2 rounded' value={quizType} onChange={changeHandler}>
+                                <option value="radio">radio</option>
+                                <option value="checkbox">checkbox</option>
+                                <option value="text">text</option>
+                            </select>
+                            <div className='mt-5'>
+                                <input type={quizType} name="quiz-option1" placeholder={quiz.option1} />
+                                <input type={quizType} name="quiz-option2" placeholder={quiz.option2} />
+                                <input type={quizType} name="quiz-option3" placeholder={quiz.option3} />
+                                <input type={quizType} name="quiz-option4" placeholder={quiz.option4} />
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -8,13 +8,13 @@ const Homepage = () => {
         <div className='container mb-15'>
             <h1 className='title text-5xl mb-15'>Project Quiz</h1>
             <Link to="/create-quiz" className='mt-10 border px-5 py-3 rounded text-blue-400 active:text-blue-600' >Create New QUIZ</Link>
-            <div className='quiz-group rounded'>
-                <div className="mx-15 py-5 mt-15">
-                    {data.map((question, index) => {
+            <div className='quiz-group bg-primary rounded'>
+                <div className="lg:mx-25 md:mx-15 mx-5 mt-15 py-5">
+                    {data.map((question) => {
                         return (
                             <div className='flex align-items-center mb-5'>
                                 <h1 className='inline my-auto'>{question.title}</h1>
-                                <Link to={`/solve-quiz/${question.id}`} className=' ms-auto border px-5 py-3 rounded text-blue-400 active:text-blue-600'>Solve Me</Link >
+                                <Link to={`/solve-quiz/${question.id}`} className=' ms-auto border md:px-5 px-3 md:py-3 py-1 rounded text-blue-400 active:text-blue-600'>Solve Me</Link >
                             </div>
                         )
                     })}
